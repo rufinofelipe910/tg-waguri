f3c92933e9e1ce8720d622413cce81c5eaba1225
Carlos <supernumerovirtual@gmail.com>
2026-06-13 11:47:04 -0400

    Crear search-ba.js

---
+++ plugins/search-ba.js
@@ -0,0 +1,22 @@
+export default {
+  help: ["ba"],
+  tags: ["search"],
+  command: ["ba"],
+
+  run: async (ctx) => {
+    const messageId = ctx.message?.message_id
+
+    try {
+      const url = "https://nexevo-m2zx.onrender.com/random/ba"
+
+      await ctx.replyWithPhoto(url, {
+        caption: "✨ Aquí tienes.",
+        reply_to_message_id: messageId
+      })
+
+    } catch (e) {
+      console.error(e)
+      await ctx.reply("❌ Error.", { reply_to_message_id: messageId })
+    }
+  }
+}
