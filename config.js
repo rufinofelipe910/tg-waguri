ca71ee7d1c6ccf3c85c73231ef3198cc22195e34
Carlos <supernumerovirtual@gmail.com>
2026-06-14 21:59:52 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.BOT_TOKEN = process.env.BOT_TOKEN || "AQUI-TOKEN-BOT"
 global.PREFIX = "/"
 
 global.owner = ["@fg_log"] //-usuario
-global.ownerID = ["8356741852"]
+global.ownerID = ["8356741852", "5877817364"]
 
 global.api = "https://nexevo-m2zx.onrender.com"
 global.apikey = ""
