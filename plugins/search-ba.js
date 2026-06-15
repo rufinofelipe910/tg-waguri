b1cca60c70f91d5b9d2b3752f11993eceb89d26e
Carlos <supernumerovirtual@gmail.com>
2026-06-14 21:52:47 -0400

    Actualizar search-ba.js

--- plugins/search-ba.js
+++ plugins/search-ba.js
@@ -7,13 +7,13 @@ export default {
     const messageId = ctx.message?.message_id
 
     try {
-      const url = "https://nexevo-m2zx.onrender.com/random/ba"
-
-      await ctx.replyWithPhoto(url, {
-        caption: "✨ Aquí tienes.",
-        reply_to_message_id: messageId
-      })
-
+      await ctx.replyWithPhoto(
+        `${global.api}/random/ba`,
+        {
+          caption: "✨ Aquí tienes.",
+          reply_to_message_id: messageId
+        }
+      )
     } catch (e) {
       console.error(e)
       await ctx.reply("❌ Error.", { reply_to_message_id: messageId })
