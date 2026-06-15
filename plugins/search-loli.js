fc9e3fc1d9a050117bc16d671642fad81bd797bd
Carlos <supernumerovirtual@gmail.com>
2026-06-14 21:48:50 -0400

    Actualizar search-loli.js

--- plugins/search-loli.js
+++ plugins/search-loli.js
@@ -7,13 +7,13 @@ export default {
     const messageId = ctx.message?.message_id
 
     try {
-      const url = `${global.api}/random/loli`
-
-      await ctx.replyWithPhoto(url, {
-        caption: "✨ Aquí tienes.",
-        reply_to_message_id: messageId
-      })
-
+      await ctx.replyWithPhoto(
+        `${global.api}/random/loli`,
+        {
+          caption: "✨ Aquí tienes.",
+          reply_to_message_id: messageId
+        }
+      )
     } catch (e) {
       console.error(e)
       await ctx.reply("❌ Error.", { reply_to_message_id: messageId })
