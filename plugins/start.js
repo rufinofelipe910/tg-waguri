6d0bad3b5ba1e5be0fb25f182c9dca5445b72466
Carlos <supernumerovirtual@gmail.com>
2026-06-19 13:26:38 -0400

    Actualizar start.js

--- plugins/start.js
+++ plugins/start.js
@@ -1,16 +1,26 @@
 export default {
+  command: ["start"],
 
-command: ["start"],
+  run: async (ctx, { conn, usedPrefix }) => {
+    const pushName = ctx.from.first_name || ctx.from.username || "Usuario";
+    let pp = './src/fg_logo.jpg';
 
-run: async (ctx, { conn, usedPrefix }) => {
-
-conn.reply(`
-📜 Hola estamos de prueba
-  
-Owner ${global.owner}
-${usedPrefix}help  para ver el menu del bot
-`)
-
-}
-
+    await conn.sendMessage(ctx.chat, {
+      image: { url: pp },
+      caption: `
+┌─⊷ Bienvenido ${pushName}
+▢ ${usedPrefix}help » Ver menú del bot
+▢ Owner » ${global.owner}
+▢ Estado » Fase de prueba
+└───────────
+`,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "📢 Seguir Canal", url: "https://t.me/fg_canal" }
+          ]
+        ]
+      }
+    }, { quoted: ctx });
+  }
 }
