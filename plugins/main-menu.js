ba7671cb0c1c194155e7ab3f4ed2545c713b275e
𝓡𝓮𝔂 𝓡𝓾𝓯𝓲𝓷𝓸 👑 <rufinofelipe910@gmail.com>
2026-07-02 15:21:50 +0100

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,24 +1,10 @@
-// main-menu.js - Waguri Bot (Telegram)
-// Estilo visual inspirado en el menú Waguri
-
 export default {
-  help: ["menu"],
-  tags: ["main"],
-  command: ["menu", "help", "menú", "waguri", "menucompleto", "comandos", "allmenu"],
+
+  help: ["help"],
+  tags: ["main"], 
+  command: ["menu","help","menú"],
 
   run: async (ctx, { conn, usedPrefix }) => {
-    let plugins = Object.values(global.plugins)
-    let categorias = {}
-
-    for (let plugin of plugins) {
-      if (!plugin.help || !plugin.tags) continue
-      for (let tag of plugin.tags) {
-        if (!categorias[tag]) categorias[tag] = []
-        for (let help of plugin.help) {
-          categorias[tag].push(help)
-        }
-      }
-    }
 
     let tags = {
       main: 'HERRAMIENTAS',
@@ -31,20 +17,41 @@ export default {
       tools: 'UTILIDADES'
     }
 
+    let plugins = Object.values(global.plugins)
+
+    let categorias = {}
+
+    for (let plugin of plugins) {
+
+      if (!plugin.help || !plugin.tags) continue
+
+      for (let tag of plugin.tags) {
+
+        if (!categorias[tag]) categorias[tag] = []
+
+        for (let help of plugin.help) {
+          categorias[tag].push(help)
+        }
+
+      }
+
+    }
+
     let text = `
 ✿°•  𝗪𝗔𝗚𝗨𝗥𝗜 𝗕𝗢𝗧  •°✿
 ⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑
-🌸 ¡Hola @${ctx.from.username || ctx.from.first_name || 'Usuario'}! ⸜(｡˃ᵕ˂)⸝♡
+🌸 ¡Hola! ⸜(｡˃ᵕ˂)⸝♡
 ⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑
 ⚙️ Plugins: ${plugins.length}
 
-Grupo: ${global.group}
-Canal: ${global.canal}
+Grupo: ${global.fg_group}
+Canal: ${global.fg_canal}
 
 ≡ LISTA DE MENUS
 `
 
     for (let tag in tags) {
+
       if (!categorias[tag]) continue
 
       text += `\n💖 *${tags[tag]}*\n`
@@ -52,19 +59,19 @@ Canal: ${global.canal}
       for (let cmd of categorias[tag]) {
         text += `🌈 ${usedPrefix}${cmd}\n`
       }
+
     }
 
     text += `\n⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑`
 
-    // Enviar como foto con caption (Telegraf)
+    //---
     let pp = './src/foto.jpg'
-    try {
-      await ctx.replyWithPhoto(
-        { source: pp },
-        { caption: text, parse_mode: 'Markdown' }
-      )
-    } catch (e) {
-      await ctx.reply(text, { parse_mode: 'Markdown' })
-    }
+    await conn.replyWithPhoto(
+      { source: pp },
+      { caption: text }
+    )
+    //conn.reply(text)
+
   }
+
 }
